7a854940d4715f02977bdf231530f048f820c85c
Danylo <danylohenrique2407@gmail.com>
2026-04-27 20:02:57 -0300

    feat(middleware-verifyJWT): implementation of the verification token and usage in routes

--- src/domain/check-in/http/controller/routes.ts
+++ src/domain/check-in/http/controller/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
+import { verifyJWT } from '@/middleware/verify-jwt'
 import { checkInController } from './check-in-controller'
 
 export const checkInRoutes: Router = Router()
 
-checkInRoutes.post('/', checkInController)
+checkInRoutes.post('/', verifyJWT, checkInController)
--- src/domain/shelter/http/controller/routes.ts
+++ src/domain/shelter/http/controller/routes.ts
@@ -3,9 +3,10 @@ import { Router } from 'express'
 import { nearbyController } from '@/domain/shelter/http/controller/nearby.controller'
 import { createController } from '@/domain/shelter/http/controller/register.controller'
 import { searchController } from '@/domain/shelter/http/controller/search.controller'
+import { verifyJWT } from '@/middleware/verify-jwt'
 
 export const shelterRoutes: Router = Router()
 
 shelterRoutes.get('/nearby', nearbyController)
 shelterRoutes.get('/search', searchController)
-shelterRoutes.post('/', createController)
+shelterRoutes.post('/', verifyJWT, createController)
